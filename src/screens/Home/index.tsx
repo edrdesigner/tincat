f899e694f8c36298a06d8e981742084e2a1d61cc
Eduardo Reichert <edrdesigner@gmail.com>
2022-04-21 13:59:37 -0300

    fix: home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,9 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { MaterialIcons } from '@expo/vector-icons';
-import { useTheme } from 'styled-components/native';
+import React from 'react';
 import { Container } from './styles';
-import { Text } from 'react-native';
 import { TopNavbar } from '@components/TopNavbar';
 import { HomeCards } from './components/HomeCards/HomeCards';
 
 export function Home() {
-  const { COLORS } = useTheme();
-
   return (
     <Container>
       <TopNavbar />
